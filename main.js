9f479a81b3c0e5c16a522ab632d9dabd8ef8aaf5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 16:10:58 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+                                                     
+//Note: This is for version 3.5.0. You can change it here \/
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
